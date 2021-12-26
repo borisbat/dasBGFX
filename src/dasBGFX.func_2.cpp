5e69ad7896628003982ab9bde8794492331305c3
Boris Batkin <bbatkin@gmail.com>
2021-12-25 23:25:17 -0800

    with the latest generator (and minor manual changes)

--- src/dasBGFX.func_2.cpp
+++ src/dasBGFX.func_2.cpp
@@ -11,42 +11,42 @@
 #include "need_dasBGFX.h"
 namespace das {
 void Module_dasBGFX::initFunctions_2() {
-	addExtern<bgfx_renderer_type_t (*)(),bgfx_get_renderer_type>(*this,lib,"bgfx_get_renderer_type",SideEffects::worstDefault,"bgfx_get_renderer_type");
-	addExtern<const bgfx_caps_s * (*)(),bgfx_get_caps>(*this,lib,"bgfx_get_caps",SideEffects::worstDefault,"bgfx_get_caps");
-	addExtern<const bgfx_stats_s * (*)(),bgfx_get_stats>(*this,lib,"bgfx_get_stats",SideEffects::worstDefault,"bgfx_get_stats");
-	addExtern<const bgfx_memory_s * (*)(unsigned int),bgfx_alloc>(*this,lib,"bgfx_alloc",SideEffects::worstDefault,"bgfx_alloc")
+	addExtern< bgfx_renderer_type (*)() , bgfx_get_renderer_type >(*this,lib,"bgfx_get_renderer_type",SideEffects::worstDefault,"bgfx_get_renderer_type");
+	addExtern< const bgfx_caps_s * (*)() , bgfx_get_caps >(*this,lib,"bgfx_get_caps",SideEffects::worstDefault,"bgfx_get_caps");
+	addExtern< const bgfx_stats_s * (*)() , bgfx_get_stats >(*this,lib,"bgfx_get_stats",SideEffects::worstDefault,"bgfx_get_stats");
+	addExtern< const bgfx_memory_s * (*)(unsigned int) , bgfx_alloc >(*this,lib,"bgfx_alloc",SideEffects::worstDefault,"bgfx_alloc")
 		->args({"_size"});
-	addExtern<const bgfx_memory_s * (*)(const void *,unsigned int),bgfx_copy>(*this,lib,"bgfx_copy",SideEffects::worstDefault,"bgfx_copy")
+	addExtern< const bgfx_memory_s * (*)(const void *,unsigned int) , bgfx_copy >(*this,lib,"bgfx_copy",SideEffects::worstDefault,"bgfx_copy")
 		->args({"_data","_size"});
-	addExtern<const bgfx_memory_s * (*)(const void *,unsigned int),bgfx_make_ref>(*this,lib,"bgfx_make_ref",SideEffects::worstDefault,"bgfx_make_ref")
+	addExtern< const bgfx_memory_s * (*)(const void *,unsigned int) , bgfx_make_ref >(*this,lib,"bgfx_make_ref",SideEffects::worstDefault,"bgfx_make_ref")
 		->args({"_data","_size"});
-	addExtern<void (*)(unsigned int),bgfx_set_debug>(*this,lib,"bgfx_set_debug",SideEffects::worstDefault,"bgfx_set_debug")
+	addExtern< void (*)(unsigned int) , bgfx_set_debug >(*this,lib,"bgfx_set_debug",SideEffects::worstDefault,"bgfx_set_debug")
 		->args({"_debug"});
-	addExtern<void (*)(unsigned char,bool),bgfx_dbg_text_clear>(*this,lib,"bgfx_dbg_text_clear",SideEffects::worstDefault,"bgfx_dbg_text_clear")
+	addExtern< void (*)(unsigned char,bool) , bgfx_dbg_text_clear >(*this,lib,"bgfx_dbg_text_clear",SideEffects::worstDefault,"bgfx_dbg_text_clear")
 		->args({"_attr","_small"});
-	addExtern<void (*)(unsigned short,unsigned short,unsigned short,unsigned short,const void *,unsigned short),bgfx_dbg_text_image>(*this,lib,"bgfx_dbg_text_image",SideEffects::worstDefault,"bgfx_dbg_text_image")
+	addExtern< void (*)(unsigned short,unsigned short,unsigned short,unsigned short,const void *,unsigned short) , bgfx_dbg_text_image >(*this,lib,"bgfx_dbg_text_image",SideEffects::worstDefault,"bgfx_dbg_text_image")
 		->args({"_x","_y","_width","_height","_data","_pitch"});
-	addExtern<bgfx_index_buffer_handle_s (*)(const bgfx_memory_s *,unsigned short),bgfx_create_index_buffer,SimNode_ExtFuncCallAndCopyOrMove>(*this,lib,"bgfx_create_index_buffer",SideEffects::worstDefault,"bgfx_create_index_buffer")
+	addExtern< bgfx_index_buffer_handle_s (*)(const bgfx_memory_s *,unsigned short) , bgfx_create_index_buffer ,SimNode_ExtFuncCallAndCopyOrMove>(*this,lib,"bgfx_create_index_buffer",SideEffects::worstDefault,"bgfx_create_index_buffer")
 		->args({"_mem","_flags"});
-	addExtern<void (*)(bgfx_index_buffer_handle_s,const char *,int),bgfx_set_index_buffer_name>(*this,lib,"bgfx_set_index_buffer_name",SideEffects::worstDefault,"bgfx_set_index_buffer_name")
+	addExtern< void (*)(bgfx_index_buffer_handle_s,const char *,int) , bgfx_set_index_buffer_name >(*this,lib,"bgfx_set_index_buffer_name",SideEffects::worstDefault,"bgfx_set_index_buffer_name")
 		->args({"_handle","_name","_len"});
-	addExtern<void (*)(bgfx_index_buffer_handle_s),bgfx_destroy_index_buffer>(*this,lib,"bgfx_destroy_index_buffer",SideEffects::worstDefault,"bgfx_destroy_index_buffer")
+	addExtern< void (*)(bgfx_index_buffer_handle_s) , bgfx_destroy_index_buffer >(*this,lib,"bgfx_destroy_index_buffer",SideEffects::worstDefault,"bgfx_destroy_index_buffer")
 		->args({"_handle"});
-	addExtern<bgfx_vertex_layout_handle_s (*)(const bgfx_vertex_layout_s *),bgfx_create_vertex_layout,SimNode_ExtFuncCallAndCopyOrMove>(*this,lib,"bgfx_create_vertex_layout",SideEffects::worstDefault,"bgfx_create_vertex_layout")
+	addExtern< bgfx_vertex_layout_handle_s (*)(const bgfx_vertex_layout_s *) , bgfx_create_vertex_layout ,SimNode_ExtFuncCallAndCopyOrMove>(*this,lib,"bgfx_create_vertex_layout",SideEffects::worstDefault,"bgfx_create_vertex_layout")
 		->args({"_layout"});
-	addExtern<void (*)(bgfx_vertex_layout_handle_s),bgfx_destroy_vertex_layout>(*this,lib,"bgfx_destroy_vertex_layout",SideEffects::worstDefault,"bgfx_destroy_vertex_layout")
+	addExtern< void (*)(bgfx_vertex_layout_handle_s) , bgfx_destroy_vertex_layout >(*this,lib,"bgfx_destroy_vertex_layout",SideEffects::worstDefault,"bgfx_destroy_vertex_layout")
 		->args({"_layoutHandle"});
-	addExtern<bgfx_vertex_buffer_handle_s (*)(const bgfx_memory_s *,const bgfx_vertex_layout_s *,unsigned short),bgfx_create_vertex_buffer,SimNode_ExtFuncCallAndCopyOrMove>(*this,lib,"bgfx_create_vertex_buffer",SideEffects::worstDefault,"bgfx_create_vertex_buffer")
+	addExtern< bgfx_vertex_buffer_handle_s (*)(const bgfx_memory_s *,const bgfx_vertex_layout_s *,unsigned short) , bgfx_create_vertex_buffer ,SimNode_ExtFuncCallAndCopyOrMove>(*this,lib,"bgfx_create_vertex_buffer",SideEffects::worstDefault,"bgfx_create_vertex_buffer")
 		->args({"_mem","_layout","_flags"});
-	addExtern<void (*)(bgfx_vertex_buffer_handle_s,const char *,int),bgfx_set_vertex_buffer_name>(*this,lib,"bgfx_set_vertex_buffer_name",SideEffects::worstDefault,"bgfx_set_vertex_buffer_name")
+	addExtern< void (*)(bgfx_vertex_buffer_handle_s,const char *,int) , bgfx_set_vertex_buffer_name >(*this,lib,"bgfx_set_vertex_buffer_name",SideEffects::worstDefault,"bgfx_set_vertex_buffer_name")
 		->args({"_handle","_name","_len"});
-	addExtern<void (*)(bgfx_vertex_buffer_handle_s),bgfx_destroy_vertex_buffer>(*this,lib,"bgfx_destroy_vertex_buffer",SideEffects::worstDefault,"bgfx_destroy_vertex_buffer")
+	addExtern< void (*)(bgfx_vertex_buffer_handle_s) , bgfx_destroy_vertex_buffer >(*this,lib,"bgfx_destroy_vertex_buffer",SideEffects::worstDefault,"bgfx_destroy_vertex_buffer")
 		->args({"_handle"});
-	addExtern<bgfx_dynamic_index_buffer_handle_s (*)(unsigned int,unsigned short),bgfx_create_dynamic_index_buffer,SimNode_ExtFuncCallAndCopyOrMove>(*this,lib,"bgfx_create_dynamic_index_buffer",SideEffects::worstDefault,"bgfx_create_dynamic_index_buffer")
+	addExtern< bgfx_dynamic_index_buffer_handle_s (*)(unsigned int,unsigned short) , bgfx_create_dynamic_index_buffer ,SimNode_ExtFuncCallAndCopyOrMove>(*this,lib,"bgfx_create_dynamic_index_buffer",SideEffects::worstDefault,"bgfx_create_dynamic_index_buffer")
 		->args({"_num","_flags"});
-	addExtern<bgfx_dynamic_index_buffer_handle_s (*)(const bgfx_memory_s *,unsigned short),bgfx_create_dynamic_index_buffer_mem,SimNode_ExtFuncCallAndCopyOrMove>(*this,lib,"bgfx_create_dynamic_index_buffer_mem",SideEffects::worstDefault,"bgfx_create_dynamic_index_buffer_mem")
+	addExtern< bgfx_dynamic_index_buffer_handle_s (*)(const bgfx_memory_s *,unsigned short) , bgfx_create_dynamic_index_buffer_mem ,SimNode_ExtFuncCallAndCopyOrMove>(*this,lib,"bgfx_create_dynamic_index_buffer_mem",SideEffects::worstDefault,"bgfx_create_dynamic_index_buffer_mem")
 		->args({"_mem","_flags"});
-	addExtern<void (*)(bgfx_dynamic_index_buffer_handle_s,unsigned int,const bgfx_memory_s *),bgfx_update_dynamic_index_buffer>(*this,lib,"bgfx_update_dynamic_index_buffer",SideEffects::worstDefault,"bgfx_update_dynamic_index_buffer")
+	addExtern< void (*)(bgfx_dynamic_index_buffer_handle_s,unsigned int,const bgfx_memory_s *) , bgfx_update_dynamic_index_buffer >(*this,lib,"bgfx_update_dynamic_index_buffer",SideEffects::worstDefault,"bgfx_update_dynamic_index_buffer")
 		->args({"_handle","_startIndex","_mem"});
 }
 }
--- src/dasBGFX.func_8.cpp
+++ src/dasBGFX.func_8.cpp
@@ -11,44 +11,44 @@
 #include "need_dasBGFX.h"
 namespace das {
 void Module_dasBGFX::initFunctions_8() {
-	addExtern<void (*)(bgfx_encoder_s *,unsigned short,bgfx_program_handle_s,unsigned int,unsigned int,unsigned int,unsigned char),bgfx_encoder_dispatch>(*this,lib,"bgfx_encoder_dispatch",SideEffects::worstDefault,"bgfx_encoder_dispatch")
+	addExtern< void (*)(bgfx_encoder_s *,unsigned short,bgfx_program_handle_s,unsigned int,unsigned int,unsigned int,unsigned char) , bgfx_encoder_dispatch >(*this,lib,"bgfx_encoder_dispatch",SideEffects::worstDefault,"bgfx_encoder_dispatch")
 		->args({"_this","_id","_program","_numX","_numY","_numZ","_flags"});
-	addExtern<void (*)(bgfx_encoder_s *,unsigned short,bgfx_program_handle_s,bgfx_indirect_buffer_handle_s,unsigned short,unsigned short,unsigned char),bgfx_encoder_dispatch_indirect>(*this,lib,"bgfx_encoder_dispatch_indirect",SideEffects::worstDefault,"bgfx_encoder_dispatch_indirect")
+	addExtern< void (*)(bgfx_encoder_s *,unsigned short,bgfx_program_handle_s,bgfx_indirect_buffer_handle_s,unsigned short,unsigned short,unsigned char) , bgfx_encoder_dispatch_indirect >(*this,lib,"bgfx_encoder_dispatch_indirect",SideEffects::worstDefault,"bgfx_encoder_dispatch_indirect")
 		->args({"_this","_id","_program","_indirectHandle","_start","_num","_flags"});
-	addExtern<void (*)(bgfx_encoder_s *,unsigned char),bgfx_encoder_discard>(*this,lib,"bgfx_encoder_discard",SideEffects::worstDefault,"bgfx_encoder_discard")
+	addExtern< void (*)(bgfx_encoder_s *,unsigned char) , bgfx_encoder_discard >(*this,lib,"bgfx_encoder_discard",SideEffects::worstDefault,"bgfx_encoder_discard")
 		->args({"_this","_flags"});
-	addExtern<void (*)(bgfx_encoder_s *,unsigned short,bgfx_texture_handle_s,unsigned char,unsigned short,unsigned short,unsigned short,bgfx_texture_handle_s,unsigned char,unsigned short,unsigned short,unsigned short,unsigned short,unsigned short,unsigned short),bgfx_encoder_blit>(*this,lib,"bgfx_encoder_blit",SideEffects::worstDefault,"bgfx_encoder_blit")
+	addExtern< void (*)(bgfx_encoder_s *,unsigned short,bgfx_texture_handle_s,unsigned char,unsigned short,unsigned short,unsigned short,bgfx_texture_handle_s,unsigned char,unsigned short,unsigned short,unsigned short,unsigned short,unsigned short,unsigned short) , bgfx_encoder_blit >(*this,lib,"bgfx_encoder_blit",SideEffects::worstDefault,"bgfx_encoder_blit")
 		->args({"_this","_id","_dst","_dstMip","_dstX","_dstY","_dstZ","_src","_srcMip","_srcX","_srcY","_srcZ","_width","_height","_depth"});
-	addExtern<void (*)(bgfx_frame_buffer_handle_s,const char *),bgfx_request_screen_shot>(*this,lib,"bgfx_request_screen_shot",SideEffects::worstDefault,"bgfx_request_screen_shot")
+	addExtern< void (*)(bgfx_frame_buffer_handle_s,const char *) , bgfx_request_screen_shot >(*this,lib,"bgfx_request_screen_shot",SideEffects::worstDefault,"bgfx_request_screen_shot")
 		->args({"_handle","_filePath"});
-	addExtern<bgfx_render_frame_t (*)(int),bgfx_render_frame>(*this,lib,"bgfx_render_frame",SideEffects::worstDefault,"bgfx_render_frame")
+	addExtern< bgfx_render_frame_t (*)(int) , bgfx_render_frame >(*this,lib,"bgfx_render_frame",SideEffects::worstDefault,"bgfx_render_frame")
 		->args({"_msecs"});
-	addExtern<void (*)(const bgfx_platform_data_s *),bgfx_set_platform_data>(*this,lib,"bgfx_set_platform_data",SideEffects::worstDefault,"bgfx_set_platform_data")
+	addExtern< void (*)(const bgfx_platform_data_s *) , bgfx_set_platform_data >(*this,lib,"bgfx_set_platform_data",SideEffects::worstDefault,"bgfx_set_platform_data")
 		->args({"_data"});
-	addExtern<const bgfx_internal_data_s * (*)(),bgfx_get_internal_data>(*this,lib,"bgfx_get_internal_data",SideEffects::worstDefault,"bgfx_get_internal_data");
-	addExtern<unsigned long long (*)(bgfx_texture_handle_s,unsigned long long),bgfx_override_internal_texture_ptr>(*this,lib,"bgfx_override_internal_texture_ptr",SideEffects::worstDefault,"bgfx_override_internal_texture_ptr")
+	addExtern< const bgfx_internal_data_s * (*)() , bgfx_get_internal_data >(*this,lib,"bgfx_get_internal_data",SideEffects::worstDefault,"bgfx_get_internal_data");
+	addExtern< unsigned long long (*)(bgfx_texture_handle_s,unsigned long long) , bgfx_override_internal_texture_ptr >(*this,lib,"bgfx_override_internal_texture_ptr",SideEffects::worstDefault,"bgfx_override_internal_texture_ptr")
 		->args({"_handle","_ptr"});
-	addExtern<unsigned long long (*)(bgfx_texture_handle_s,unsigned short,unsigned short,unsigned char,bgfx_texture_format_t,unsigned long long),bgfx_override_internal_texture>(*this,lib,"bgfx_override_internal_texture",SideEffects::worstDefault,"bgfx_override_internal_texture")
+	addExtern< unsigned long long (*)(bgfx_texture_handle_s,unsigned short,unsigned short,unsigned char,bgfx_texture_format,unsigned long long) , bgfx_override_internal_texture >(*this,lib,"bgfx_override_internal_texture",SideEffects::worstDefault,"bgfx_override_internal_texture")
 		->args({"_handle","_width","_height","_numMips","_format","_flags"});
-	addExtern<void (*)(const char *),bgfx_set_marker>(*this,lib,"bgfx_set_marker",SideEffects::worstDefault,"bgfx_set_marker")
+	addExtern< void (*)(const char *) , bgfx_set_marker >(*this,lib,"bgfx_set_marker",SideEffects::worstDefault,"bgfx_set_marker")
 		->args({"_marker"});
-	addExtern<void (*)(unsigned long long,unsigned int),bgfx_set_state>(*this,lib,"bgfx_set_state",SideEffects::worstDefault,"bgfx_set_state")
+	addExtern< void (*)(unsigned long long,unsigned int) , bgfx_set_state >(*this,lib,"bgfx_set_state",SideEffects::worstDefault,"bgfx_set_state")
 		->args({"_state","_rgba"});
-	addExtern<void (*)(bgfx_occlusion_query_handle_s,bool),bgfx_set_condition>(*this,lib,"bgfx_set_condition",SideEffects::worstDefault,"bgfx_set_condition")
+	addExtern< void (*)(bgfx_occlusion_query_handle_s,bool) , bgfx_set_condition >(*this,lib,"bgfx_set_condition",SideEffects::worstDefault,"bgfx_set_condition")
 		->args({"_handle","_visible"});
-	addExtern<void (*)(unsigned int,unsigned int),bgfx_set_stencil>(*this,lib,"bgfx_set_stencil",SideEffects::worstDefault,"bgfx_set_stencil")
+	addExtern< void (*)(unsigned int,unsigned int) , bgfx_set_stencil >(*this,lib,"bgfx_set_stencil",SideEffects::worstDefault,"bgfx_set_stencil")
 		->args({"_fstencil","_bstencil"});
-	addExtern<unsigned short (*)(unsigned short,unsigned short,unsigned short,unsigned short),bgfx_set_scissor>(*this,lib,"bgfx_set_scissor",SideEffects::worstDefault,"bgfx_set_scissor")
+	addExtern< unsigned short (*)(unsigned short,unsigned short,unsigned short,unsigned short) , bgfx_set_scissor >(*this,lib,"bgfx_set_scissor",SideEffects::worstDefault,"bgfx_set_scissor")
 		->args({"_x","_y","_width","_height"});
-	addExtern<void (*)(unsigned short),bgfx_set_scissor_cached>(*this,lib,"bgfx_set_scissor_cached",SideEffects::worstDefault,"bgfx_set_scissor_cached")
+	addExtern< void (*)(unsigned short) , bgfx_set_scissor_cached >(*this,lib,"bgfx_set_scissor_cached",SideEffects::worstDefault,"bgfx_set_scissor_cached")
 		->args({"_cache"});
-	addExtern<unsigned int (*)(const void *,unsigned short),bgfx_set_transform>(*this,lib,"bgfx_set_transform",SideEffects::worstDefault,"bgfx_set_transform")
+	addExtern< unsigned int (*)(const void *,unsigned short) , bgfx_set_transform >(*this,lib,"bgfx_set_transform",SideEffects::worstDefault,"bgfx_set_transform")
 		->args({"_mtx","_num"});
-	addExtern<void (*)(unsigned int,unsigned short),bgfx_set_transform_cached>(*this,lib,"bgfx_set_transform_cached",SideEffects::worstDefault,"bgfx_set_transform_cached")
+	addExtern< void (*)(unsigned int,unsigned short) , bgfx_set_transform_cached >(*this,lib,"bgfx_set_transform_cached",SideEffects::worstDefault,"bgfx_set_transform_cached")
 		->args({"_cache","_num"});
-	addExtern<unsigned int (*)(bgfx_transform_s *,unsigned short),bgfx_alloc_transform>(*this,lib,"bgfx_alloc_transform",SideEffects::worstDefault,"bgfx_alloc_transform")
+	addExtern< unsigned int (*)(bgfx_transform_s *,unsigned short) , bgfx_alloc_transform >(*this,lib,"bgfx_alloc_transform",SideEffects::worstDefault,"bgfx_alloc_transform")
 		->args({"_transform","_num"});
-	addExtern<void (*)(bgfx_uniform_handle_s,const void *,unsigned short),bgfx_set_uniform>(*this,lib,"bgfx_set_uniform",SideEffects::worstDefault,"bgfx_set_uniform")
+	addExtern< void (*)(bgfx_uniform_handle_s,const void *,unsigned short) , bgfx_set_uniform >(*this,lib,"bgfx_set_uniform",SideEffects::worstDefault,"bgfx_set_uniform")
 		->args({"_handle","_value","_num"});
 }
 }
